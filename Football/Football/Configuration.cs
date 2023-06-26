995e05eeffacd7bf47ed26b60767cd39c77e0d5b
GorobchikMaxim <pupezm@gmail.com>
2023-06-26 08:50:03 +0200

    Commit#1

---
+++ Football/Football/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace FootballBetting
+{
+    public class Configuration
+    {
+        public const string ConnectionString = @"Data Source=FSB\MYSERVER;Database=FootballBetting;Integrated Security=true;Encrypt=false;TrustServerCertificate=true;";
+    }
+}
